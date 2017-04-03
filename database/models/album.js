fe30c1ec5aa2a1db71b7da72a88072331eebc028
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-04-03 18:16:22 +0200

    Added the album model

--- database/models/album.js
+++ database/models/album.js
@@ -1,1 +1,14 @@
-// Todo: create Album Schema
+const mongoose = require('mongoose');
+
+const albumSchema = new mongoose.Schema({
+    title: String,
+    date: Date,
+    copiesSold: Number,
+    numberTracks: Number,
+    image: String,
+    revenue: Number
+});
+
+const Album = mongoose.model('Album', albumSchema);
+
+module.exports = Album;
