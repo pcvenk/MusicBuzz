a416d8d49b9259f97e8ee7dc8ba70ab667d0f1ad
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-04-04 17:17:32 +0200

    Min and max years active search filter done

--- database/queries/GetYearsActiveRange.js
+++ database/queries/GetYearsActiveRange.js
@@ -6,4 +6,23 @@ const Artist = require('../models/artist');
  * containing the min and max yearsActive, like { min: 0, max: 14 }.
  */
 module.exports = () => {
+    let minYears = Artist
+        .find({})
+        .sort({yearsActive: 1})
+        .limit(1)
+        .then(result => result[0].yearsActive);
+
+    let maxYears = Artist
+        .find({})
+        .sort({yearsActive: -1})
+        .limit(1)
+        .then(result => result[0].yearsActive);
+
+    return Promise.all([minYears, maxYears])
+        .then((result) => {
+            return {
+                min: result[0],
+                max: result[1]
+            };
+        });
 };
